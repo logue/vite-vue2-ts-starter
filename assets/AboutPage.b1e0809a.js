99b50092ab682f468f60ec8d8e9d3d8e59bdd979
logue <logue@hotmail.co.jp>
2022-08-26 11:33:09 +0000

    Deploying to gh-pages from @ logue/vite-vue2-ts-starter@628b4a7a51878893ef732d04a483470d5ae3b44d 🚀

---
+++ assets/AboutPage.b1e0809a.js
@@ -0,0 +1,1 @@
+import{d as s}from"./vue.1280e55e.js";import{n as t}from"./index.58e290bb.js";const r=s({});var _=function(){var n=this;return n._self._c,n._self._setupProxy,n._m(0)},o=[function(){var e=this,n=e._self._c;return e._self._setupProxy,n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],a=t(r,_,o,!1,null,null,null,null);const i=a.exports;export{i as default};
