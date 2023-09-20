0fa24f36e14c2214890c47c28e5e334808b737d3
logue <logue@hotmail.co.jp>
2023-09-20 00:01:56 +0000

    Deploying to gh-pages from @ logue/vite-vue2-ts-starter@367641f77f228f7cf09c6f8c86eecf513f043b29 🚀

---
+++ assets/AboutView-b31873bf.js
@@ -0,0 +1,1 @@
+import{d as _}from"./vue-4f2ff8f0.js";import{n as t}from"./index-7a7b8359.js";const s=_({});var o=function(){var e=this;return e._self._c,e._self._setupProxy,e._m(0)},r=[function(){var n=this,e=n._self._c;return n._self._setupProxy,e("div",{staticClass:"about"},[e("h1",[n._v("This is an about page")])])}],a=t(s,o,r,!1,null,null,null,null);const u=a.exports;export{u as default};
